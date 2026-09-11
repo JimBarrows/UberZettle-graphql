1dc6dd19d6961b306783bce0303584d3950d1da1
Jim Barrows <JIm.Barrows@gmail.com>
2026-09-10 21:56:33 -0500

    Should be the resource annotation

--- src/behaviorTest/java/com/nsfwllc/uberzettlegraphql/RunCucumberTests.java
+++ src/behaviorTest/java/com/nsfwllc/uberzettlegraphql/RunCucumberTests.java
@@ -6,13 +6,13 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("com.nsfwllc.uberzettlegraphql")
+@SelectClasspathResource("com.nsfwllc.uberzettlegraphql")
 @ConfigurationParameters({
 		// Targets the exact directory for your feature files
 		@ConfigurationParameter(key = "cucumber.features", value = "src/behaviorTest/resources/features"),
 
 		// Points Cucumber to the package where your Step Definitions are defined
-		@ConfigurationParameter(key = "cucumber.glue", value = "com.nsfwllc.uberzettlegraphql")
+		@ConfigurationParameter(key = "cucumber.glue", value = "com.nsfwllc.uberzettlegraphql.steps")
 })
 public class RunCucumberTests {
 }
